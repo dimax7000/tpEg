823959628e01270ba588ef996a1c1c998138ed8e
dimax700 <komlanvidanklou@gmail.com>
2022-05-27 13:57:18 +0000

    tp des boucles

---
+++ boucle.js
@@ -0,0 +1,20 @@
+var name = prompt(" votre nom svp:");
+alert(name);
+
+var entrerNote = prompt("Veuillez entrer vos notes");
+
+
+if(entrerNote <= note.length){
+    
+  alert ("les nombres de notes n'est pas atteint");
+
+
+}else{
+  var moyen = entrerNote /note.length;
+  alert(`Votre moyenne : ${moyen`);
+}
+
+
+
+
+
